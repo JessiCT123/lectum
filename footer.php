e79a0c3dd3f6c5310f133bc82060e32eab31f87a
JessiCT123 <1874565@alu.murciaeduca.es>
2026-04-14 15:48:31 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+        <footer>
+            <p>Lectum &copy; proyecto fin de grado 2025/2026</p>
+            
+        </footer>
+        <a id="btn-fixed" href="https://web.whatsapp.com/">
+            <img src="https://cdn.pixabay.com/photo/2015/08/03/13/58/soon-873316_960_720.png" alt="Whatsapp" width="20"
+                height="20" />
+        </a>
+</body>
+
+</html>
