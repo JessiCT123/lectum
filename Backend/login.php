fcabd169cf587e59ad626d037332e3bc257cd980
Nesbpf <2006868@alu.murciaeduca.es>
2026-04-15 20:13:06 +0200

    Actualización sesion y login

--- Backend/login.php
+++ Backend/login.php
@@ -49,9 +49,8 @@ try {
                 setcookie("remember_token", $token, [
                     'expires' => time() + (86400 * 15),
                     'path' => '/',
-                    'httponly' => true,
-                    'samesite' => 'Strict'
-                    // 'secure' => true // activar si usas HTTPS
+                    'httponly' => true, // No se puede acceder a la cookie desde JavaScript
+                    'samesite' => 'Strict' // Evita que la cookie se envíe en peticiones externas 
                 ]);
             }
 
